2b7a5cf013b84ac36f99033a2bba7e76c06ad361
Bao NGUYEN QUOC <bao.nguyen-quoc-externe@soget.fr>
2026-07-09 11:26:10 +0700

    init chapter 2

---
+++ src/chapter-2/1-cholesky-decomposition.js
@@ -0,0 +1,3 @@
+// Cho ma trận A kích thước 3*3. Thực hiện phân rã ma trận bằng phương pháp Cholesky:
+// - Cổ điển
+// - Biến thể
---
+++ src/chapter-2/2-matrix-diagonalization.js
@@ -0,0 +1,1 @@
+// Cho ma trận A kích thước 3*3, thực hiện chéo hóa ma trận
